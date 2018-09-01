8596e3ef43ecc1bdb5809810bc2156ba132fb887
Deluxateer <deluxateer@gmail.com>
2018-08-31 21:08:35 -0400

    adds bootstrap with styles, and static prop data to comps

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
   render() {
+    const { name, email, phone } = this.props;
+
     return (
-      <div>
-        <h4>John Doe</h4>
-        <ul>
-          <li>Email: jdoe@gmail.com</li>
-          <li>555-555-5555</li>
+      <div className="card card-body mb-3">
+        <h4>{name}</h4>
+        <ul className="list-group">
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     );
   }
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
 
 export default Contact;
